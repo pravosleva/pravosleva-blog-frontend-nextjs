dc9e0cf8dbb8e70649313a47f5a90de1d8fcf44b
Den Pol <selection4test@gmail.com>
2020-01-08 04:00:56 +0300

    master. dotenv added

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const withCSS = require('@zeit/next-css');
+const { parsed: localEnv } = require('dotenv').config();
 
 
 const nextConfig = {
   webpack (config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+
     return config
   },
   devIndicators: {
