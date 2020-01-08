bf3caccbe2cfa39cc4a1373585b0e0f98fe2da02
Den Pol <selection4test@gmail.com>
2020-01-08 04:42:38 +0300

    master. fix

--- pages/_document.js
+++ pages/_document.js
@@ -78,8 +78,9 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossOrigin="anonymous" />
           <link href='https://fonts.googleapis.com/css?family=Montserrat:400,500' rel='stylesheet' />
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossOrigin="anonymous" />
+
           {/* Global Site Tag (gtag.js) - Google Analytics
             isProduction && (
               <>
