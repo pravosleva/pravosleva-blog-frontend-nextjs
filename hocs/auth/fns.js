7a049ab5e906c19e6e541552a7c5990073525b42
Den Pol <selection4test@gmail.com>
2020-03-09 23:22:03 +0300

    issue-2-with-cookie-auth. fix

--- hocs/auth/fns.js
+++ hocs/auth/fns.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 const isDev = process.env.NODE_ENV === 'development';
 const baseURL = isDev
   ? 'http://localhost:1337'
-  : 'http://80.87.194.181';
+  : 'http://80.87.194.181/api';
 const api = axios.create({ baseURL });
 
 export const login = async ({ jwt }) => {
