53568c1c78289b2803a1732e25bf316b2e2794ce
Den Pol <selection4test@gmail.com>
2020-06-06 17:43:50 +0300

    issue-2-with-cookie-auth. fix

--- hocs/with-socket-api.js
+++ hocs/with-socket-api.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import Head from 'next/head';
 import io from 'socket.io-client';
 // import { getMyOrders, getTheOrder } from './with-auth-once';
 import { usersActions } from '../store/reducer/users';
 import { getSocketApiUrl } from '../lib/getApiUrl';
+// import dynamic from 'next/dynamic'
 
 const socketApiUrl = getSocketApiUrl();
 const getUsersArr = users => Object.keys(users).map((key, i, a) => ({ socketId: key, ip: users[key].ip, geo: users[key].geo }));
@@ -13,85 +15,92 @@ export const withSocketApi = WrappedComponent => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-      const client = io.connect(socketApiUrl);
-      // const id = user.fromServer ? user.fromServer.id : null;
+      if (process.browser) {
+        const client = io.connect(socketApiUrl);
+        // const id = user.fromServer ? user.fromServer.id : null;
 
-      client.on('HELLO', payload => {
-        const { users } = payload;
+        client.on('HELLO', payload => {
+          const { users } = payload;
 
-        console.log(payload)
+          console.log(payload)
 
-        if (!!users) {
-          dispatch(usersActions.set(getUsersArr(users)));
+          if (!!users) {
+            dispatch(usersActions.set(getUsersArr(users)));
+          }
+        });
+        client.on('ARTICLE_UPDATED', payload => {
+          console.log(payload);
+          // TODO: dispatch could be used...
+        });
+        client.on('SOMEBODY_CONNECTED', payload => {
+          const { users } = payload;
+
+          if (!!users) {
+            dispatch(usersActions.set(getUsersArr(users)));
+          }
+        });
+        client.on('SOMEBODY_RENNECTED', payload => {
+          const { users } = payload;
+
+          if (!!users) {
+            dispatch(usersActions.set(getUsersArr(users)));
+          }
+        });
+        client.on('SOMEBODY_DISCONNECTED', payload => {
+          const { users } = payload;
+
+          if (!!users) {
+            dispatch(usersActions.set(getUsersArr(users)));
+          }
+        });
+        /*
+        client.on('ORDER_UPDATED', payload => {
+          console.log(payload);
+          if (!payload.id) return;
+
+          // console.log(myOrdersList);
+
+          // const id = props.cookies.get('userId');
+          const jwt = props.cookies.get('jwt');
+
+          // Step 1: Need to requset for the order if necessary...
+          const isThisCurrentArticle = [...myOrdersList].some(o => o.id == payload.id); // eslint-disable-line
+          // NOTE: Нестрогое равенство из-за багов Strapi@beta
+
+          if (isThisCurrentArticle) {
+            // Step 2: For current user case.
+
+            // Step 2.1: GET /orders/:id
+            getTheOrder({ jwt, id: payload.id })
+              .then(res => {
+                // Step 2.2: Result received. Then we have to update it in redux store...
+                // TODO: Преобразовать объект?
+
+                dispatch(myOrdersActions.update({ ...res }));
+              })
+              .catch(err => {
+                console.log('Result crashed.');
+                console.log(err);
+              });
+          } else {
+            console.log('socket emit ORDER_UPDATED ignored: Not my Order updated.');
+          }
+        });
+        */
+
+        return () => {
+          client.disconnect();
+          client.removeAllListeners();
         }
-      });
-      client.on('ARTICLE_UPDATED', payload => {
-        console.log(payload);
-        // TODO: dispatch could be used...
-      });
-      client.on('SOMEBODY_CONNECTED', payload => {
-        const { users } = payload;
-
-        if (!!users) {
-          dispatch(usersActions.set(getUsersArr(users)));
-        }
-      });
-      client.on('SOMEBODY_RENNECTED', payload => {
-        const { users } = payload;
-
-        if (!!users) {
-          dispatch(usersActions.set(getUsersArr(users)));
-        }
-      });
-      client.on('SOMEBODY_DISCONNECTED', payload => {
-        const { users } = payload;
-
-        if (!!users) {
-          dispatch(usersActions.set(getUsersArr(users)));
-        }
-      });
-      /*
-      client.on('ORDER_UPDATED', payload => {
-        console.log(payload);
-        if (!payload.id) return;
-
-        // console.log(myOrdersList);
-
-        // const id = props.cookies.get('userId');
-        const jwt = props.cookies.get('jwt');
-
-        // Step 1: Need to requset for the order if necessary...
-        const isThisCurrentArticle = [...myOrdersList].some(o => o.id == payload.id); // eslint-disable-line
-        // NOTE: Нестрогое равенство из-за багов Strapi@beta
-
-        if (isThisCurrentArticle) {
-          // Step 2: For current user case.
-
-          // Step 2.1: GET /orders/:id
-          getTheOrder({ jwt, id: payload.id })
-            .then(res => {
-              // Step 2.2: Result received. Then we have to update it in redux store...
-              // TODO: Преобразовать объект?
-
-              dispatch(myOrdersActions.update({ ...res }));
-            })
-            .catch(err => {
-              console.log('Result crashed.');
-              console.log(err);
-            });
-        } else {
-          console.log('socket emit ORDER_UPDATED ignored: Not my Order updated.');
-        }
-      });
-      */
-
-      return () => {
-        client.disconnect();
-        client.removeAllListeners();
       }
-    }, []);
+    }, [process.browser]);
 
-    return <WrappedComponent {...props} />;
+    return (
+      <>
+
+        <WrappedComponent {...props} />
+      </>
+    );
   }
 
   return Wrapper;
