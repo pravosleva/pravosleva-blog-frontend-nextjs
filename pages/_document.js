5b17df9f37ce38356119eb3c11348bb9eef80d66
Den Pol <selection4test@gmail.com>
2020-06-13 14:46:07 +0300

    master. fix

--- pages/_document.js
+++ pages/_document.js
@@ -73,7 +73,6 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta name="theme-color" content="#0162c8" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-          <link href="/static/prismjs/themes/prism.css" rel="stylesheet" />
           <link href="/static/prismjs/themes/prism-okaidia.css" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500" rel="stylesheet" />
           <link
