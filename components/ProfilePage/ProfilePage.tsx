a1cc5e08257b1b695c490bfdeb2a5cf2f7781cb8
Den Pol <selection4test@gmail.com>
2020-07-01 03:27:53 +0300

    master. prismjs hl added

--- components/ProfilePage/ProfilePage.tsx
+++ components/ProfilePage/ProfilePage.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import styled from 'styled-components'
 // import { useSelector } from 'react-redux'
 // import { IRootState } from '@/store/reducers/IRootState'
 import { IUserInfo } from '@/helpers/services/IUserInfo'
 import { AuthToken } from '@/helpers/services/AuthToken'
+import Prism from 'prismjs'
 
 const Container = styled('div')`
   @media (min-width: 768px) {
@@ -30,11 +31,18 @@ export const ProfilePage: React.FC<IProps> = (props) => {
     console.log(`REACT_APP_SOCKET_ENDPOINT: ${process.env.REACT_APP_SOCKET_ENDPOINT}`)
     console.log(`RECAPTCHAV3_VERIFY_URL: ${process.env.RECAPTCHAV3_VERIFY_URL}`)
   }, [])
+  useEffect(() => {
+    // You can call the Prism.js API here
+    // Use setTimeout to push onto callback queue so it runs after the DOM is updated
+    setTimeout(() => Prism.highlightAll(), 0)
+  }, [])
 
   return (
     <Container>
       <h2>Profile</h2>
-      <pre>{JSON.stringify(props.userInfo, null, 2)}</pre>
+      <pre>
+        <code className="language-json">{JSON.stringify(props.userInfo, null, 2)}</code>
+      </pre>
       <button className="rippled-btn" style={{ width: 'auto', background: 'gray' }} onClick={showEnvs}>
         <span>
           <i className="fas fa-terminal" style={{ marginRight: '10px' }}></i>
